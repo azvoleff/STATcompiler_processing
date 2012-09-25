b0c683128bb845721c468b91b018b48328040c2b
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-25 14:58:37 -0700

    Fix for new SC.

--- 5_make_region_list.R
+++ 5_make_region_list.R
@@ -9,9 +9,9 @@ merged_data_dir <- paste(base_dir, "Merged_data", sep="/")
 
 load(paste(merged_data_dir, "DHS_merged_data_regional.Rdata", sep="/"))
 
-regions_list <- regional_data[c("CountryISO", "Survey", "Survey_Year", "GISDataYr", "Group.Type", "Group", "Group.sub", "Group.lowest")]
+regions_list <- regional_data[c("CC_3", "Survey", "Year", "GISDataYr", "Category", "Characteristic", "Characteristic.parent")]
 
-new_order <- with(regions_list, order(CountryISO, Survey, Survey_Year, Group.Type, Group, Group.sub))
+new_order <- with(regions_list, order(CC_3, Survey, Year, Category, Characteristic, Characteristic.parent))
 regions_list <- regions_list[new_order,]
 
 write.csv(regions_list,
