084a0584013c250fc0174ad754c73ce9444641a3
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-25 14:56:47 -0700

    Fix for new SC.

--- 3_survey_summary.R
+++ 3_survey_summary.R
@@ -12,9 +12,9 @@ load(paste(merged_data_dir, "DHS_merged_data.Rdata", sep="/"))
 
 countries <- data.frame(Country=unique(merged_data$Country))
 continents <- merged_data$Continent[match(countries$Country, merged_data$Country)]
-ISO <- merged_data$CountryISO[match(countries$Country, merged_data$Country)]
+CC_3 <- merged_data$CC_3[match(countries$Country, merged_data$Country)]
 
-countries <- cbind(countries, Continent=continents, ISO=ISO,
+countries <- cbind(countries, Continent=continents, CC_3=CC_3,
         NumSurveys=rep(0, nrow(countries)), Survey1=rep(NA, nrow(countries)),
         Survey2=rep(NA, nrow(countries)), Survey3=rep(NA, nrow(countries)),
         Survey4=rep(NA, nrow(countries)), Survey5=rep(NA, nrow(countries)),
@@ -33,7 +33,7 @@ for (n in 1:nrow(countries)) {
 countries <- countries[order(countries$Continent, countries$Country),]
 
 # Reorder the factor levels before plotting
-countries$ISO <- reorder(countries$ISO, 1:nrow(countries))
+countries$CC_3 <- reorder(countries$CC_3, 1:nrow(countries))
 countries$Country <- reorder(countries$Country, 1:nrow(countries))
 countries$Continent <- reorder(countries$Continent, 1:nrow(countries))
 
@@ -47,7 +47,7 @@ write.csv(countries, file=paste(merged_data_dir,
 #write.dta(countries, file=paste(merged_data_dir, "DHS_merged_data_surveys_by_country.dta", sep="/"))
 
 require(ggplot2)
-q <- qplot(ISO, NumSurveys, geom="bar", ylab="Number of surveys",
+q <- qplot(CC_3, NumSurveys, geom="bar", ylab="Number of surveys",
         xlab="Country ISO Code", colour=Continent, data=countries)
 q + opts(axis.text.x=theme_text(angle=90, hjust=1, size=6))
 ggsave(paste(merged_data_dir, "DHS_merged_data_surveys_by_country.png", 
