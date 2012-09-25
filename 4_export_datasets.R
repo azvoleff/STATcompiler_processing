2e609edb473fa65b42d8de35374aca481d7dadfd
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-25 14:56:38 -0700

    Fix for new SC, and remove Aoife/Laura rename.

--- 4_export_datasets.R
+++ 4_export_datasets.R
@@ -18,7 +18,7 @@ load(paste(merged_data_dir, "DHS_merged_data_surveys_by_country.Rdata",
 ###############################################################################
 # Export regional_data
 ###############################################################################
-regional_data <- merged_data[merged_data$Group.Type %in% c("Region", "Sub-region"),]
+regional_data <- merged_data[merged_data$Category %in% c("Region", "Sub-region"),]
 # Remove columns that are empty in the regional only data
 empty_cols <- apply(regional_data, 2, function(col) length(which(!is.na(col)))==0)
 regional_data <- regional_data[!(empty_cols)]
@@ -27,25 +27,6 @@ save(regional_data, file=paste(merged_data_dir,
 write.csv(regional_data, file=paste(merged_data_dir, 
                                     "DHS_merged_data_regional.csv", sep="/"), 
           row.names=FALSE, na=".")
-
-# Rename the regional data variables so that the names match those used by 
-# Aoife/Laura.
-var_rename_key <- read.csv("Variable_Rename_Key/combined_variable_names.csv",
-        encoding="latin1", stringsAsFactors=FALSE)
-# Only use rows where there is a mapping both ways:
-var_rename_key <- var_rename_key[(!is.na(var_rename_key$Var.Me) &
-        !is.na(var_rename_key$Var.Laura)),]
-for (n in 1:nrow(var_rename_key)) {
-     col_num <- which(names(regional_data) == var_rename_key[n,]$Var.Me)
-     if (length(col_num) > 1) stop("Multiple matches during variable renaming")
-     names(regional_data)[col_num] <- var_rename_key[n,]$Var.Laura
- }
-# Write out the version of the data for Aoife and Laura
-write.csv(regional_data, file=paste(merged_data_dir, 
-                                    "DHS_merged_data_regional_Aoife+Laura.csv", 
-                                    sep="/"),
-          row.names=FALSE, na=".")
-#write.dta(regional_data, file="Merged_data/DHS_merged_data_regional.dta")
 
 # Make a subset of the regional data that includes only the most recent survey 
 # from each country.
@@ -60,7 +41,7 @@ write.csv(regional_data, file=paste(merged_data_dir,
 ###############################################################################
 # Export urbal/rural data 
 ###############################################################################
-urban_rural_data <- merged_data[merged_data$Group.Type=="Residence",]
+urban_rural_data <- merged_data[merged_data$Category == "Residence",]
 # Remove columns that are empty in the Urban/Rural only data
 empty_cols <- apply(urban_rural_data, 2,
         function(col) length(which(!is.na(col)))==0)
